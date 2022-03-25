e8cdf3b316b35d46d140e079f66cfa3fcb17e6fb
Auredy Sanchez <auredysanchez@gmail.com>
2022-03-25 12:42:27 -0400

    created another json file

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss';
+
+function Footer(props) {
+    return (
+        <div className='footer' >
+            This is a footer
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,32 @@
+// import React from "react";
+import React, { useState} from "react";
+import "./NavBar.scss";
+
+import menuItemData from '../../data/menuItemData.json'
+
+function NavBar() {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">Logo</div>
+      <div className={active ? "navbar__menuItems-active" : "navbar__menuItems"}>
+        <ul>
+            {menuItemData.map((menuItem, key) => {
+                return (
+                  <li key={key}>
+                    <a href={menuItem.href}>{menuItem.text}</a>
+                  </li>
+                );
+            })}
+        </ul>
+      </div>
+      <div
+        className="navbar__collapsedMenuIcon"
+        onClick={() => setActive(!active)}
+      > = </div>
+    </div>
+  );
+}
+
+export default NavBar;

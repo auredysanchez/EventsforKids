b2d0ae17a8615ab782cc18ab6c4ddbac08c26953
Auredy Sanchez <auredysanchez@gmail.com>
2022-03-25 12:55:54 -0400

    created separate file for pixels

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,8 +1,8 @@
 // import React from "react";
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./NavBar.scss";
 
-import menuItemData from '../../data/menuItemData.json'
+import menuItemData from "../../data/menuItemData.json";
 
 function NavBar() {
   const [active, setActive] = useState(false);
@@ -10,21 +10,25 @@ function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar__logo">Logo</div>
-      <div className={active ? "navbar__menuItems-active" : "navbar__menuItems"}>
+      <div
+        className={active ? "navbar__menuItems-active" : "navbar__menuItems"}
+      >
         <ul>
-            {menuItemData.map((menuItem, key) => {
-                return (
-                  <li key={key}>
-                    <a href={menuItem.href}>{menuItem.text}</a>
-                  </li>
-                );
-            })}
+          {menuItemData.map((menuItem, key) => {
+            return (
+              <li key={key}>
+                <a href={menuItem.href}>{menuItem.text}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div
         className="navbar__collapsedMenuIcon"
         onClick={() => setActive(!active)}
-      > = </div>
+      >
+          =
+      </div>
     </div>
   );
 }

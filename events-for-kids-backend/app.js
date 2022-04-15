1fefeea59f6dbf533499483a62dd0bf00f373b85
Auredy Sanchez <auredysanchez@gmail.com>
2022-04-15 00:15:25 -0400

    installed cors

--- events-for-kids-backend/app.js
+++ events-for-kids-backend/app.js
@@ -1,11 +1,14 @@
 const { response } = require("express");
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 const eventsController = require("./controllers/eventsController");
+
+app.use(cors());
 
 app.use("/events", eventsController);
 

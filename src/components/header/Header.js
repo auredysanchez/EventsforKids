cd03131e3acf462ecbe6ad0bc3e56e4b498eaf46
Auredy Sanchez <auredysanchez@gmail.com>
2022-04-27 15:51:15 -0400

    created separate component for the logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,13 @@
 import React from "react";
 import NavBar from "../navBar/NavBar";
 import SearchBar from "../searchBar/SearchBar";
+import Logo from "../logo/Logo";
 import "./Header.scss";
 
 const Header = ({ search, setSearch }) => {
   return (
     <header className="header">
-      <img src="/familyGroup.png" className="header__logo" alt="" />
-
-      {/* 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          enableBackground="new 0 0 24 24"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="#000000"
-        >
-          <g>
-            <rect fill="none" height="24" width="24" x="0" />
-            <path d="M12,8.89L12.94,12h2.82l-2.27,1.62l0.93,3.01L12,14.79l-2.42,1.84l0.93-3.01L8.24,12h2.82L12,8.89 M12,2l-2.42,8H2 l6.17,4.41L5.83,22L12,17.31L18.18,22l-2.35-7.59L22,10h-7.58L12,2L12,2z" />
-          </g>
-        </svg> */}
-      {/* <Logo /> */}
+      <Logo />
       <SearchBar search={search} setSearch={setSearch} />
       <NavBar />
     </header>

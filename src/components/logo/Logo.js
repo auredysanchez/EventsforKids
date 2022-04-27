c7d598c5526ab0fbaa5182cd2ce11b80a7a865df
Auredy Sanchez <auredysanchez@gmail.com>
2022-04-27 16:15:52 -0400

    changed version

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logo.scss";
+
+const Logo = () => {
+  return (
+    <header>
+      <img src="/familyGroup.png" className="logo" alt="" />
+    </header>
+  );
+};
+
+export default Logo;
+//className="header__logo"

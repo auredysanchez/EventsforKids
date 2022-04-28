65a593a028c5e1347454d4f68cf034768ac9eafd
Auredy Sanchez <auredysanchez@gmail.com>
2022-04-28 17:06:19 -0400

    added console log

--- src/components/eventList/EventList.js
+++ src/components/eventList/EventList.js
@@ -8,6 +8,23 @@ import EmptyList from "../emptyList/EmptyList";
 
 const EventList = ({search}) => {
     const [filteredkidsEvents, setFilteredkidsEvents] = useState([]);
+
+      useEffect(() => {
+   
+        if (search) {
+          let lowerCaseSearch = search.toLocaleLowerCase();
+          let listOfkidsEvents = kidsEvents.filter((event) => {
+            let fullEvent = `${event.title}`;
+            fullEvent = fullEvent.toLocaleLowerCase();
+            return fullEvent.includes(lowerCaseSearch);
+          });
+          setFilteredkidsEvents(listOfkidsEvents);
+        } else {
+          setFilteredkidsEvents([]);
+        }
+      }, [search]);
+
+      console.log(kidsEvents);
     return (
         <div>
             

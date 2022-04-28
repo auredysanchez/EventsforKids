144c5cf1feea3d94c1bf37a0b95596716d1e26d4
Auredy Sanchez <auredysanchez@gmail.com>
2022-04-27 20:18:40 -0400

    added hook to eventList

--- src/components/eventList/EventList.js
+++ src/components/eventList/EventList.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import EventsCard from "../eventsCard/EventsCard";
+import kidsEvents from "../../data/kidsEvents.json";
+import EmptyList from "../emptyList/EmptyList";
 
 
 
 const EventList = ({search}) => {
-
+    const [filteredkidsEvents, setFilteredkidsEvents] = useState([]);
     return (
         <div>
-
+            
         </div>
     )
 }

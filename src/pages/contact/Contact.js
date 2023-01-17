849e34dc0d37241edd3df7b25e72d7ed0c85fd95
Auredy Sanchez <auredysanchez@gmail.com>
2023-01-17 15:11:13 -0500

    email on contact component working

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -3,13 +3,20 @@ import './Contact.scss';
 
 
 const Contact = () => {
+
+
+
   return (
     <div className="contact">
       <h1>Contact</h1>
       <p>
-        <a className="mailto" href="auredysanchez@pursuit.org">
-          Auredy Sanchez - email
+        Auredy Sanchez - {' '}
+        <a href="mailto:auredysanchez@pursuit.org">
+          auredysanchez@pursuit.org
         </a>
+        {/* <a className="mailto" href="auredysanchez@pursuit.org">
+          Auredy Sanchez - auredysanchez@pursuit.org
+        </a> */}
         {/* <Button
           onClick={() => Linking.openURL("mailto:auredysanchez@pursuit.org")}
           title="auredysanchez@pursuit.org"

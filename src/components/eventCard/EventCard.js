47bda81ce1d0ee254690c614dddf143a3001209c
Auredy Sanchez <auredysanchez@gmail.com>
2023-02-01 14:24:37 -0500

    created cards in grid format.alot of styling to continue.plus other random changes

--- src/components/eventCard/EventCard.js
+++ src/components/eventCard/EventCard.js
@@ -1,18 +1,23 @@
 import React from "react";
 import './EventCard.scss'
+import EventImage from "../eventImage/EventImage";
 
 
 const EventCard = (eventsInfo) => {
-    const { title, date, location, isFree, age } = eventsInfo;
+    const { title, date, image, location, isFree, age } = eventsInfo;
 
 
   return (
     <div className="eventCard__eventInfo">
-      <div className="eventCard__eventTitle">{title}</div>
-      <div className="eventCard__eventDate">{date}</div>
-      <div className="eventCard__eventLocation">{location}</div>
-      <div className="eventCard__eventIsFree">{isFree}</div>
-      <div className="eventCard__eventAge">Age: {age[0]}-{age[1]}</div>
+      <div className="eventCard__eventTitle"> {title} </div>
+      <div className="eventCard__eventDate"> {date} </div>
+      <div className="eventCard__eventImage"> <EventImage image={image}/> </div>
+      <div className="eventCard__eventLocation"> {location} </div>
+      <div className="eventCard__eventIsFree"> {isFree} </div>
+      <div className="eventCard__eventAge">
+        {" "}
+        Age: {age[0]} - {age[1]}{" "}
+      </div>
     </div>
   );
 };
---
+++ src/components/eventImage/EventImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./EventImage.scss";
+
+const EventImage = () => {
+  return (
+      <img src="/familyGroup.png" className="eventImage" alt="image" />
+ 
+  );
+};
+
+export default EventImage;

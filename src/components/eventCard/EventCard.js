f6db6d35c61f6bc101786ab794358475036b4ba9
Auredy Sanchez <auredysanchez@gmail.com>
2023-03-09 23:02:43 -0500

    changes made on the eventcard component

--- src/components/eventCard/EventCard.js
+++ src/components/eventCard/EventCard.js
@@ -1,12 +1,19 @@
 import React from "react";
+// import SearchBar from "../searchBar/SearchBar";
+// import { useEffect, useState } from "react";
 import "./EventCard.scss";
 import EventImage from "../eventImage/EventImage";
+// import Grid from '@mui/material/Grid'
 
 const EventCard = (eventsInfo) => {
   const { title, date, image, location, isFree, age } = eventsInfo;
 
+  //  const [search, setSearch] = useState("");
+
   return (
+    // <div className="eventCard">
     <div className="eventCard">
+      {/* <Grid container spacing={1}/> */}
       <div className="eventCard__eventTitle"> {title} </div>
       <div className="eventCard__eventDate"> {date} </div>
       <div className="eventCard__eventImage">
@@ -21,8 +28,9 @@ const EventCard = (eventsInfo) => {
       <div className="eventCard__eventIsFree"> {isFree} </div>
       <div className="eventCard__eventAge">
         {" "}
-        <span className="eventCard__eventAge__color">Age:</span>{" "}
-        {age[0]} - {age[1]}{" "}
+        <span className="eventCard__eventAge__color">Age:</span> 
+        {" "} 
+        {age[0]} - {age[1]}
       </div>
     </div>
   );
